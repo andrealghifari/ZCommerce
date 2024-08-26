ed75aee79bf9f09ef8d99ef97dcd893fc95ebef5
andrealghifari <andre.alghifaric@gmail.com>
2024-08-26 12:03:04 +0700

    add success purchase info after payment

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,5 +1,15 @@
+import { Alert, AlertTitle, Box } from "@mui/material";
+
 const Confirmation = () => {
-    return <div>Transaction completed, Thank you</div>
-}
+  return (
+    <Box margin="90px auto" width="80%" height="50vh">
+      <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        You have successfully made an Order -{" "}
+        <strong>We'll be shipping and delivering to you right away!</strong>
+      </Alert>
+    </Box>
+  );
+};
 
 export default Confirmation;
